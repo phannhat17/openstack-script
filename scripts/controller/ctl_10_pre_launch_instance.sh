585e85b4d77d3aa4ca5545cb78327e3ea1712d7f
phannhat17 <phandinhnhat1708@gmail.com>
2024-11-12 22:37:00 +0700

    Update pre-launch instance script to source virtual networks from the controller directory and include admin-openrc

--- scripts/controller/ctl_10_pre_launch_instance.sh
+++ scripts/controller/ctl_10_pre_launch_instance.sh
@@ -3,11 +3,13 @@
 # Load configuration from config.cfg
 source config.cfg
 
-source ./virtual_networks/vn_01_provider.sh
-source ./virtual_networks/vn_02_selfservice.sh
+source controller/virtual_networks/vn_01_provider.sh
+source controller/virtual_networks/vn_02_selfservice.sh
 
 # Function to create the m1.nano flavor
 create_flavor() {
+    source /root/admin-openrc
+
     echo "${YELLOW}Creating m1.nano flavor...${RESET}"
     openstack flavor create --id 0 --vcpus 1 --ram 64 --disk 1 m1.nano
     echo "${GREEN}m1.nano flavor created successfully.${RESET}"
@@ -15,6 +17,8 @@ create_flavor() {
 
 # Function to generate and add SSH key pair
 create_keypair() {
+    source /root/admin-openrc
+
     echo "${YELLOW}Generating SSH key pair...${RESET}"
     
     # Check if SSH key already exists; if not, generate a new key
@@ -31,6 +35,8 @@ create_keypair() {
 
 # Function to add security group rules
 add_security_group_rules() {
+    source /root/admin-openrc
+
     echo "${YELLOW}Adding security group rules...${RESET}"
 
     # Allow ICMP (ping)
